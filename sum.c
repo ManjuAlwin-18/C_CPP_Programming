686a89045b2480ae81d1bd2040d32edeac73cdcd
Manju Davis <manjudavis18@gmail.com>
2023-02-02 11:17:17 +1300

    Intial sum file add

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    int number1, number2,sum;
+    printf("ENTER 2 NUMBERS");
+    scanf("%d%d",&number1,&number2);
+    sum=number1+number2;
+    printf("Result is : %d",sum);
+    return 0;
+}
